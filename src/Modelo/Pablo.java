406f84f8e21111cbb9c43944c3844338c26964ae
Juan <jj.monte2002@gmail.com>
2024-10-07 20:25:00 -0500

    fourth commit arrangements

--- src/Modelo/Pablo.java
+++ src/Modelo/Pablo.java
@@ -1,27 +1,32 @@
 package Modelo;
 
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
 
 public class Pablo extends Thread{
+
+    private int vuelta = 1;
+    private final ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
 
     @Override
     public void run() {
     
-        int vuelta =1;
-        System.out.println("pablo alista la burra");
-        while (vuelta<=7) {            
-            
+        System.out.println("Pablo comienza");
+        
+        scheduler.scheduleAtFixedRate(() ->{
             try {
-                Thread.sleep(1700);
-                System.out.println("pablo va por la vuelta "+vuelta);
-                vuelta++;
-                
-            } catch (InterruptedException ex) {
-            
-                System.out.println("pablo se quedo comprando weed y paila");
-                
+                if(vuelta<=7){
+                    System.out.println("pablo a por la vuelta" +vuelta);
+                    vuelta++;
+                }
+            } catch (Exception e) {
+                System.out.println("Ocurrio un error "+e.getMessage());
             }
             
-        }
+        }, 0, 1700, TimeUnit.MICROSECONDS);
+        
         System.out.println("pablo llega");
     }
     
